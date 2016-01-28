ff430c2c0768969de10e6e6e29b777be4fc1b3b4
tdesbara <thibautdesbarats@gmail.com>
2016-01-28 18:28:04 +0100

    test

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 28 janvier 2016, 18:22
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
